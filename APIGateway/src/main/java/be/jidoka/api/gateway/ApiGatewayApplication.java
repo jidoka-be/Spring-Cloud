6136842f4a3398930e5a81fb43c07f2645d70826
Martijn Haex <martijn.haex@jidoka.be>
2016-07-17 18:43:47 +0200

    API Gateway.

---
+++ APIGateway/src/main/java/be/jidoka/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,24 @@
+package be.jidoka.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+    @Bean
+    AlwaysSampler alwaysSampler() {
+        return new AlwaysSampler();
+    }
+
+}
